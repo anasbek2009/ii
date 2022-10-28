0cc56030bdcfa7f98fb0543efbfc3df775ae3bc6
John Doe <johndoe@example.com>
2022-10-28 11:30:44 +0500

    assdeqfwe

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const sidebar = document.querySelector(".sidebar")
+const vehicles = document.querySelector(".vehicles")
+const tols = document.querySelector(".tols")
 
 console.log("js");
 
 function show(){
-    console.log(1);
     sidebar.classList.toggle("sidebar_show")
+}
+function add(){
+    vehicles.classList.toggle("vehicle_showHide")
+}
+function tools(){
+    tols.classList.toggle("tols_show")
 }
